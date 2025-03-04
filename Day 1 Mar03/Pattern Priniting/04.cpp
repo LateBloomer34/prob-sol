007635b6a6bfd7a02a4acdeabc4005570acb126c
LateBloomer34 <himnariyal30@gmail.com>
2025-03-04 14:39:31 +0530

    pattern print

---
+++ Day 1 Mar03/Pattern Priniting/04.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+void pattern (int n ){ 
+for (int i = 0 ; i < n ;i++){
+for (int j = 0  ; j<=i ; j++){
+    cout<<1+i<<" ";
+}
+    cout<<endl;
+}
+}
+
+int main(){
+    int n;
+    cout<<"enter the value of n :- ";
+    cin >>n ;
+    pattern (n);
+    
+}
