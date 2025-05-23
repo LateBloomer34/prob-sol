31a7ffc2e8dfc98389918442d43ad9654e2863bf
LateBloomer34 <himnariyal30@gmail.com>
2025-05-23 10:55:19 +0530

    second largest in an array

---
+++ 01Array/01 Easy/02SecondLargestInAnArray.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+#include<bits/stdc++.h>
+
+pair <int , int> sec(vector<int>arr){
+    int largest = INT_MIN;
+    int secLar = INT_MIN;
+
+    for (int i = 0 ; i < arr.size() ; i++){
+        if (arr[i]>largest){
+            secLar = largest;
+            largest = arr[i];
+        }
+        else if (arr[i]>secLar && arr[i]<largest){
+            secLar = arr[i];
+        }
+    }
+    return {largest , secLar};
+}
+
+int main (){
+    vector<int>arr = {3,4,6,7,9,2,8,6};
+    pair<int, int>res = sec(arr);
+    cout<<res.first<<" "<<res.second;
+}
