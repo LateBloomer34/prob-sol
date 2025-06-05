5819a4bf65f5394251184b98b5aaa0a02a7ffb9f
LateBloomer34 <himnariyal30@gmail.com>
2025-06-05 12:17:09 +0530

    odd number of an array

--- 01Array/01 Easy/RN/04printOddNumbersOfArray.cpp
+++ 01Array/01 Easy/RN/04printOddNumbersOfArray.cpp
@@ -12,7 +12,7 @@ int odd(int arr[] , int n){
 
 
 int main (){
-    int arr[] = {1,2,3,4,5,6,7,8,9};
+    int arr[] = {1,2,-3,4,5,6,7,8,9};
     int n = sizeof arr/ sizeof arr[0];
     odd(arr , n);
 }
