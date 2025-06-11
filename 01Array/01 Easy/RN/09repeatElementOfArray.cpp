d83c208252da1dad311f91afa79d08810a4ba823
LateBloomer34 <himnariyal30@gmail.com>
2025-06-11 11:57:21 +0530

    non repeating elemenet of an array

--- 01Array/01 Easy/RN/09repeatElementOfArray.cpp
+++ 01Array/01 Easy/RN/09repeatElementOfArray.cpp
@@ -1,6 +1,27 @@
 #include<iostream>
 using namespace std;
 
+//brute approach
+void repeat(vector<int >arr ){
+    int n = arr.size();
+for (int i = 0 ; i < n ;i++){
+    bool repeting = true;
+    for (int j = 0 ; j < n ; j++){
+        if (i == j){
+            continue;
+        }
+        if (arr[i]== arr[j]){
+            repeting = false;
+        }
+    }
+    if (repeting){
+        cout<<arr[i]<<endl;
+    }
+}
+}
+
+//optimal approach
+/*
 int repeat(vector<int >arr ){
     int ele = 0;
     for (int i = 0 ; i < arr.size() ; i++){
@@ -10,6 +31,7 @@ int repeat(vector<int >arr ){
 
     return 0;
 }
+*/
 
 int main (){
     vector<int>arr = {1,2,3,5,5,4,3,2,1};
